fe23cf8fed3b5b24f71443d32780acb0db804a50
elvedin123 <elvedinc97@gmail.com>
2022-02-02 14:28:40 -0500

    working on homepage

--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -1,7 +1,13 @@
 import './Home.module.css'
 import  Layout  from '../../components/Layout/Layout.jsx'
+import {useState, useEffect} from "React"
 
 const Home = (props) => {
+  const [games, setGames] = useState([]);
+  // useEffect(()=>{
+
+  // },[])
+
   return (
     <Layout >
       <div className='home'>
