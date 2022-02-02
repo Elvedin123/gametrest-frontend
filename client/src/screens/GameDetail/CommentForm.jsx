e081acf3bbdc5da37dd011b1e4f8bc01e1157af1
Hampton Jacobs <hamptonjacobs@Hannahs-Air.fios-router.home>
2022-02-02 16:39:24 -0500

    modified the edit and create

--- client/src/screens/GameDetail/CommentForm.jsx
+++ client/src/screens/GameDetail/CommentForm.jsx
@@ -1,22 +1,25 @@
 import { useState } from 'react';
+import Layout from '../../components/Layout/Layout.jsx';
+
+const default_input = {
+  comment: "",
+}
 
 export default function CommentForm() {
   // Schema calls for game, comment, and likes. I am assuming we just want to  edit the comment
-  const [comment, setComment] = useState({
-    comment: "",
-  })
+  const [comment, setComment] = useState(default_input)
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    await createComment(id, comment) //Create Comment come from api
+    await createComment(comment) //Create Comment come from api
   }
 
   const handleChange = (event) => {
     const { id, value } = event.target;
-    setComment({
-      ...comment,
+    setComment(prevInput => ({
+      ...prevInput,
       [id]: value,
-    })
+    }))
   }
 
   return (
