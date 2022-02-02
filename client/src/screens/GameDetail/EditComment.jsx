f570f79001a5d591916e1a81d2e867363e89d09a
Hampton Jacobs <hamptonjacobs@Hannahs-Air.fios-router.home>
2022-02-02 16:11:43 -0500

    Created edit for comments

--- client/src/screens/GameDetail/EditComment.jsx
+++ client/src/screens/GameDetail/EditComment.jsx
@@ -1,5 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 export default function EditComment() {
+  const [comment, setComment] = useState({
+    comment: "",
+  })
+
+  let { id } = useParams();
+
+  useEffect(() => {
+    const fetchComment = async () => {
+      const comment = await getComment(id) //Depends on ConfigApi?
+      setComment(comment)
+    }
+    fetchComment();
+  }, []);
+
+  const handleChange = (event) => {
+    const { id, value } = event.target;
+    setComment({
+      ...comment,
+      [id]: value,
+    })
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    await updateComment(id, comment) //Also from api??
+
+  }
+
   return <div></div>;
 }
