2fd868cfaf898609ebbaae392031f913d0665b4f
Virginie Bonhomme <virginiebonhomme@Virginies-MBP.home>
2022-02-02 11:10:42 -0500

    signup default new user

--- client/src/SigninForm.jsx
+++ client/src/SigninForm.jsx
@@ -1,6 +1,22 @@
 import React from 'react';
+import { useState } from "react"
+
+
+defaultNewUser = {
+  userName: "",
+  firstName: "",
+  lastName: "",
+  email: "",
+  password: "",
+  confirmPassword: "",
+}
 
 function SigninForm() {
+  const [newUser, setNewUser] = useState(defaultNewUser)
+
+
+
+
   return (<div>
 
   </div>)
