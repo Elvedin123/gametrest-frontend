fa294cc1ebeed34aa52afe171e468d59411a290d
Hampton Jacobs <hamptonjacobs@Hannahs-Air.fios-router.home>
2022-02-02 16:30:01 -0500

    updated edit comment

--- client/src/screens/GameDetail/EditComment.jsx
+++ client/src/screens/GameDetail/EditComment.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect } from 'react';
 
+const default_input = {
+  comment: "",
+}
+
 export default function EditComment() {
-  const [comment, setComment] = useState({
-    comment: "",
-  })
+  const [comment, setComment] = useState(default_input);
 
   let { id } = useParams();
 
@@ -13,14 +15,14 @@ export default function EditComment() {
       setComment(comment)
     }
     fetchComment();
-  }, []);
+  }, [id]);
 
   const handleChange = (event) => {
     const { id, value } = event.target;
-    setComment({
-      ...comment,
+    setComment(prevInput => ({
+      ...prevInput,
       [id]: value,
-    })
+    }))
   }
 
   const handleSubmit = (event) => {
