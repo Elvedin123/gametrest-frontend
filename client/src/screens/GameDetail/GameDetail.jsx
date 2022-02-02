ca1874f1347b113d5bcf0656cafc69aed6e83fed
Hampton Jacobs <hamptonjacobs@Hannahs-Air.fios-router.home>
2022-02-02 14:12:28 -0500

    Detail page mostly done

--- client/src/screens/GameDetail/GameDetail.jsx
+++ client/src/screens/GameDetail/GameDetail.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { Layout } from '../../components/Layout/Layout.jsx';
 
