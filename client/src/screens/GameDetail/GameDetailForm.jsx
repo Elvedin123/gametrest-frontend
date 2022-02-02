d79bfd1992e98d92fa896b334ba3b248045ae401
Hampton Jacobs <hamptonjacobs@Hannahs-Air.fios-router.home>
2022-02-02 14:55:05 -0500

    adding to the form for game details

---
+++ client/src/screens/GameDetail/GameDetailForm.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+export default function GameDetailForm() {
+  // Schema calls for game, comment, and likes. I am assuming we just want to  edit the comment
+  const [comment, setComment] = useState({
+    comment: "",
+  })
+
+  let { id } = useParams();
+
+  useEffect(() => {
+    const fetchGame = async () => {
+      const comment = await getGame(id)
+      setComment(comment)
+    }
+    fetchGame();
+  }, []);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    await updateComment(id, comment)
+  }
+
+  const handleChange = (event) => {
+    const { id, value } = event.target;
+    setComment({
+      ...comment,
+      [id]: value,
+    })
+  }
+
+  return (
+    <div>
+
+    </div>);
+}
