f5bbf5d25a07fad5c71df890597751b59565d69b
Virginie Bonhomme <virginiebonhomme@Virginies-MBP.home>
2022-02-02 11:20:59 -0500

    password check

--- client/src/SigninForm.jsx
+++ client/src/SigninForm.jsx
@@ -13,8 +13,23 @@ defaultNewUser = {
 
 function SigninForm() {
   const [newUser, setNewUser] = useState(defaultNewUser)
+  const [validationMessage, setValidationMessage] = useState("");
+  const [valid, setValid] = useState(false);
 
-
+  const checkIfValid = () => {
+    if (newUser.password === "" || newUser.confirmPassword === "") {
+      setValidationMessage("");
+      setValid(false);
+    } else if (newUser.password.length < 9) {
+      setValidationMessage("Short password! Must be at least 8 characters");
+    } else if (newUser.password !== newUser.confirmPassword) {
+      setValidationMessage("Password does not match!");
+      setValid(false);
+    } else {
+      setValidationMessage("Password match!");
+      setValid(true);
+    }
+  };
 
 
   return (<div>
