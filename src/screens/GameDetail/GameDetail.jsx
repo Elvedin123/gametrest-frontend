96d886f85a231321a8ee4cbe965fb182788161b0
Hampton Jacobs <hamptonjacobs@Hannahs-Air.fios-router.home>
2022-02-03 10:30:23 -0500

    added some imports

--- src/screens/GameDetail/GameDetail.jsx
+++ src/screens/GameDetail/GameDetail.jsx
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { Layout } from '../../components/Layout/Layout.jsx';
+// import { EditComment } from './EditComment'
+// import { CommentForm } from './CommentForm'
+// import { DeleteComment } from './DeleteComment'
 
 export default function GameDetail() {
   const [game, setGames] = useState(null);
