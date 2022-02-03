8ab2be6fd3ec78039e16bbc0272fea694e028217
elvedin123 <elvedinc97@gmail.com>
2022-02-03 10:10:26 -0500

    fixing api key

--- src/services/gamesApi.js
+++ src/services/gamesApi.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const API_KEY = `${process.env.REACT_APP_API_KEY}`
+const API_KEY = process.env.REACT_APP_API_KEY
 const gameUrl = {
 
   production: `https://api.rawg.io/api/games`,
