43ced086ae5c9f9859c7dd0b32cf5b9dafe75cb7
Virginie Bonhomme <virginiebonhomme@Virginies-MBP.home>
2022-02-02 21:12:55 -0500

    fixed merge

--- src/screens/Login/LoginForm.jsx
+++ src/screens/Login/LoginForm.jsx
@@ -58,7 +58,7 @@ function LoginForm(props) {
           id="password"
           value={password}
           placeholder="password"
-          onChange={handelChange}
+          onChange={handleChange}
         />
         <br />
         {handleError()}
