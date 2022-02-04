1ab712811cd73e850e69a70a6f0d7a649e2636d1
Hampton Jacobs <hamptonjacobs@Hannahs-Air.fios-router.home>
2022-02-04 17:28:24 -0500

    added spacing and added a GAMEterest to lead us home

--- src/screens/Signup/SignupForm.jsx
+++ src/screens/Signup/SignupForm.jsx
@@ -1,12 +1,7 @@
 import React, { useCallback, useState, useEffect } from 'react';
 import { createUser } from "../../services/apiConfig.js";
-import { useNavigate } from "react-router-dom"
+import { useNavigate, Link } from "react-router-dom"
 import signupcss from './SignupForm.module.css'
-
-// import { library } from '@fortawesome/fontawesome-svg-core';
-// import { fas } from '@fortawesome/free-solid-svg-icons';
-// import { fab } from '@fortawesome/free-brands-svg-icons';
-// library.add(fas, fab);
 
 const defaultNewUser = {
   userName: "",
@@ -75,8 +70,7 @@ function SignupForm(props) {
 
   return (
     <div>
-      <i className="fas fa-arrow-left"></i>
-      <h2 className={signupcss.back_arrow}></h2>
+      <Link className={signupcss.back_arrow} to='/'>GameTerest</Link>
       <h3 className={signupcss.header}>Free Sign Up</h3>
       <form
         className={signupcss.form_body}
