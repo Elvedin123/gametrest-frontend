dc094491d372a4a948a9a9c550cee8b169fa4b24
Hampton Jacobs <hamptonjacobs@Hannahs-Air.fios-router.home>
2022-02-06 15:28:05 -0500

    Avatar stuff

--- src/screens/GameDetail/Comments/DeleteComment.jsx
+++ src/screens/GameDetail/Comments/DeleteComment.jsx
@@ -1,12 +1,13 @@
 import { deleteUserComments } from "../../../services/apiConfig.js";
 // import { useNavigate } from "react-router";
 
+// const userId = localStorage.getItem('userId')
 
 export default function DeleteComment() {
   // const navigate = useNavigate()
 
   const handleDelete = async () => {
-    await deleteUserComments();
+    // await deleteUserComments(userId, id);
     console.log(deleteUserComments)
     // navigate("/profile")
 
